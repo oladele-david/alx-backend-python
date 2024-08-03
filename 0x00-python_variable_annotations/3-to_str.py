1fad0624de3a83a667f156116c97d3f34c668d3f
oladele.david <tdboy59@gmail.com>
2024-08-03 11:46:11 +0100

    Initial commit task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotated function to_str that takes a float n as argument and returns
+the string representation of the float."""
+from typing import Union
+
+
+def to_str(n: Union[int, float]) -> str:
+    """Return the string representation of the float."""
+    return str(n)
